0b6c8ac3e8dae6c0410fb405bce9730e48f084b6
Sergey Zhilin <solomonsdt93@gmail.com>
2020-03-06 22:07:45 +0300

    add styles for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+import './style.css'
+
 
 export class Header extends Component {
   state = {
@@ -8,16 +13,18 @@ export class Header extends Component {
   
   render() {
     return (
-      <header>
-        <p className="header-logo">sadasad</p>
+      <header className='header' >
+        <p className="header-logo">Эльдорадо</p>
         <div className='header-profile'>
-          <img onClick={this.toggleDropdown} src='https://cdn.cnn.com/cnnnext/dam/assets/180226110801-elettro-domestici-logo.jpg' alt='avatar'></img>
-          <span>userName@mail.com</span>
+          <img  className='header-img' src='https://cdn.cnn.com/cnnnext/dam/assets/180226110801-elettro-domestici-logo.jpg' alt='avatar'/>
+          <span className='header-userName'>userName@mail.com</span>
+          <FontAwesomeIcon icon={faEllipsisV} className='header-more-icon' onClick={this.toggleDropdown} />
           {
             this.state.showDropDown == true ? (
-              <div>
-                <p>Профиль</p>
-                <p>Настройки</p>
+              <div className='header-dropdown'>
+                <p className='dropdown-link'>Профиль</p>
+                <p className='dropdown-link'>Настройки</p>
+                <Link to='/' className='dropdown-link'>Выход</Link>
               </div>
             ) : null
           }
