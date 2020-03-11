cd5555865ecf358d92e6d0b76fbbc68570b7c7b9
SergeyZhilin93 <solomonsdt93@gmail.com>
2020-03-11 17:50:20 +0300

    added complete and delete method for Task

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,11 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import { api } from '../../api'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenSquare, faTrashAlt, faCalendarCheck } from '@fortawesome/free-solid-svg-icons'
+import './style.css'
 
 export class Task extends Component {
   
   state = {
     inputValue: '',
-    formActive: false
+    formActive: false,
   }
 
   handleChange = e => {
@@ -21,13 +24,42 @@ export class Task extends Component {
     e.preventDefault()
     api.put(`/tasks/${this.props.data.id}`, {name: this.state.inputValue, completed: this.props.data.completed })
   }
+
+  handleDelete = e => {
+    e.preventDefault()
+    api.delete(`/tasks/${this.props.data.id}`)
+  }
+
+  handleComplete = e => {
+    e.preventDefault()
+    api.put(`/tasks/${this.props.data.id}`, {name: this.props.data.name, completed: !this.props.data.completed })
+  }
   
   render() {
     return(
       <Fragment >
         <div>
-          <p>{`${this.props.index} ${this.props.data.name}`}</p>
-          <button onClick={this.toggleForm} className='updateTask-button'>Изменить</button>
+          {
+            this.props.data.completed ? 
+            (
+              <div>
+                <p className='completed-task'>{`${this.props.index} ${this.props.data.name}`}</p>
+                <button className='deleteTask-button' onClick={this.handleDelete}>Удалить</button>
+              </div>
+            )
+            : 
+            (
+              <div>
+                <p>{`${this.props.index} ${this.props.data.name}`}</p>
+                <button onClick={this.toggleForm} className='updateTask-button'>Изменить</button>
+                <button className='deleteTask-button' onClick={this.handleDelete}>Удалить</button>
+              </div>
+            )
+          }
+          {/* <p>{`${this.props.index} ${this.props.data.name}`}</p> */}
+          {/* <button onClick={this.toggleForm} className='updateTask-button'>Изменить</button>
+          <button className='deleteTask-button' onClick={this.handleDelete}>Удалить</button> */}
+          {/* <button onClick={this.handleComplete}>com</button> */}
         </div>
         {
           this.state.formActive ?
@@ -40,6 +72,7 @@ export class Task extends Component {
               className='task-update' 
               defaultValue={this.props.data.name}></input>
               <button onClick={this.handleSubmit} className='task-update-button'>Изменить задание</button>
+              <button onClick={this.handleComplete}>Complete</button>
             </div>
           )
           : null
